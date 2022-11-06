12b9cadeff46362a38141cbac0efc2f69b56b6b7
Alimul Hassan <alhassan069@gmail.com>
2022-11-07 00:18:40 +0530

    removed env

--- routes/message.js
+++ routes/message.js
@@ -5,12 +5,12 @@ const router = require("express").Router();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-
+const twilioSid = process.env.TWILIO_SID;
 router.post('/send', async(req, res) => {
     const { phone, message } = req.body;
     client.messages.create({
         body: message,
-        messagingServiceSid: 'MG34e63d9ec2b30f1095e10f6d06523877',
+        messagingServiceSid: twilioSid,
         to: '+91' + phone,
     }).then((message) => {
         let dbMessage = {...req.body, status: message.status };
